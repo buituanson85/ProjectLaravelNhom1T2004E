11287d6b4303ea538637dce43695173041ad7c2c
buituan son <buituanson85@outlook.com.vn>
2021-04-16 01:34:48 +0700

    Chốt Migration + Model + Controller

--- resources/views/layouts/Backend/function.blade.php
+++ resources/views/layouts/Backend/function.blade.php
@@ -1,48 +1,132 @@
-<div id="main-menu" class="main-menu collapse navbar-collapse">
-    <ul class="nav navbar-nav">
-        <li class="active">
-            <a href="{{ route('dashboard.index') }}"> <i class="fa fa-dashboard"></i>&nbsp;&nbsp;&nbsp;Dashboard </a>
-        </li>
-        @foreach($roles as $role)
-            @foreach($role->users as $user)
-                @if(Auth::user()->id == $user->id)
-                    <h3 class="menu-title">{{ $role->title }}</h3><!-- /.menu-title -->
-                    @if(count($role->permissions)>0)
-                        @foreach($role->permissions as $permission)
-                            @if($permission ->name != "Logout")
-                                <li class="menu-item-has-children dropdown">
-                                    @if($permission->parent == 0)
-                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">&nbsp;&nbsp;&nbsp;{!! $permission->icon !!}&nbsp;&nbsp;&nbsp;{{ $permission->name }}</a>
-                                        <ul class="sub-menu children dropdown-menu">
-                                            @foreach($role->permissions as $permission_c)
-                                                @if($permission_c->parent == $permission->id )
-                                                    @if($permission_c ->name != "Logout")
-                                                    <li><a href="{{ route( $permission_c->url ) }}">{!! $permission_c->icon !!}{{ $permission_c->name }}</a></li>
-                                                    @else
-                                                    <li>
-                                                        <a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();"><i class="fas fa-sign-out-alt"></i><span class="ml-1">{{ __('Logout') }}</span></a>
-                                                    </li>
-                                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                                        @csrf
-                                                    </form>
-                                                    @endif
-                                                @endif
-                                            @endforeach
-                                        </ul>
-                                    @endif
-                                </li>
-                            @else
+<aside id="left-panel" class="left-panel">
+    <nav class="navbar navbar-expand-sm navbar-default">
 
-                            @endif
-                        @endforeach
-                    @else
+        <div id="main-menu" class="main-menu collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active">
+                    <a href="index.html"> <i class="menu-icon fa fa-dashboard"></i>Dashboard </a>
+                </li>
+                <h3 class="menu-title">UI elements</h3><!-- /.menu-title -->
+                <li class="menu-item-has-children dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-laptop"></i>Components</a>
+                    <ul class="sub-menu children dropdown-menu">
+                        <li><i class="fa fa-puzzle-piece"></i><a href="ui-buttons.html">Buttons</a></li>
+                        <li><i class="fa fa-id-badge"></i><a href="ui-badges.html">Badges</a></li>
+                        <li><i class="fa fa-bars"></i><a href="ui-tabs.html">Tabs</a></li>
+                        <li><i class="fa fa-share-square-o"></i><a href="ui-social-buttons.html">Social Buttons</a></li>
+                        <li><i class="fa fa-id-card-o"></i><a href="ui-cards.html">Cards</a></li>
+                        <li><i class="fa fa-exclamation-triangle"></i><a href="ui-alerts.html">Alerts</a></li>
+                        <li><i class="fa fa-spinner"></i><a href="ui-progressbar.html">Progress Bars</a></li>
+                        <li><i class="fa fa-fire"></i><a href="ui-modals.html">Modals</a></li>
+                        <li><i class="fa fa-book"></i><a href="ui-switches.html">Switches</a></li>
+                        <li><i class="fa fa-th"></i><a href="ui-grids.html">Grids</a></li>
+                        <li><i class="fa fa-file-word-o"></i><a href="ui-typgraphy.html">Typography</a></li>
+                    </ul>
+                </li>
+                <li class="menu-item-has-children dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-table"></i>Tables</a>
+                    <ul class="sub-menu children dropdown-menu">
+                        <li><i class="fa fa-table"></i><a href="tables-basic.html">Basic Table</a></li>
+                        <li><i class="fa fa-table"></i><a href="tables-data.html">Data Table</a></li>
+                    </ul>
+                </li>
+                <li class="menu-item-has-children dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-th"></i>Forms</a>
+                    <ul class="sub-menu children dropdown-menu">
+                        <li><i class="menu-icon fa fa-th"></i><a href="forms-basic.html">Basic Form</a></li>
+                        <li><i class="menu-icon fa fa-th"></i><a href="forms-advanced.html">Advanced Form</a></li>
+                    </ul>
+                </li>
 
-                    @endif
+                <h3 class="menu-title">Icons</h3><!-- /.menu-title -->
 
-                @else
+                <li class="menu-item-has-children dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-tasks"></i>Icons</a>
+                    <ul class="sub-menu children dropdown-menu">
+                        <li><i class="menu-icon fa fa-fort-awesome"></i><a href="font-fontawesome.html">Font Awesome</a></li>
+                        <li><i class="menu-icon ti-themify-logo"></i><a href="font-themify.html">Themefy Icons</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="widgets.html"> <i class="menu-icon ti-email"></i>Widgets </a>
+                </li>
+                <li class="menu-item-has-children dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-bar-chart"></i>Charts</a>
+                    <ul class="sub-menu children dropdown-menu">
+                        <li><i class="menu-icon fa fa-line-chart"></i><a href="charts-chartjs.html">Chart JS</a></li>
+                        <li><i class="menu-icon fa fa-area-chart"></i><a href="charts-flot.html">Flot Chart</a></li>
+                        <li><i class="menu-icon fa fa-pie-chart"></i><a href="charts-peity.html">Peity Chart</a></li>
+                    </ul>
+                </li>
 
-                @endif
-            @endforeach
-        @endforeach
-    </ul>
-</div><!-- /.navbar-collapse -->
+                <li class="menu-item-has-children dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-area-chart"></i>Maps</a>
+                    <ul class="sub-menu children dropdown-menu">
+                        <li><i class="menu-icon fa fa-map-o"></i><a href="maps-gmap.html">Google Maps</a></li>
+                        <li><i class="menu-icon fa fa-street-view"></i><a href="maps-vector.html">Vector Maps</a></li>
+                    </ul>
+                </li>
+                <h3 class="menu-title">Extras</h3><!-- /.menu-title -->
+                <li class="menu-item-has-children dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-glass"></i>Pages</a>
+                    <ul class="sub-menu children dropdown-menu">
+                        <li><i class="menu-icon fa fa-sign-in"></i><a href="page-login.html">Login</a></li>
+                        <li><i class="menu-icon fa fa-sign-in"></i><a href="page-register.html">Register</a></li>
+                        <li><i class="menu-icon fa fa-paper-plane"></i><a href="pages-forget.html">Forget Pass</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </nav>
+</aside>
+
+
+
+{{--<div id="main-menu" class="main-menu collapse navbar-collapse">--}}
+{{--    <ul class="nav navbar-nav">--}}
+{{--        <li class="active">--}}
+{{--            <a href="{{ route('dashboard.index') }}"> <i class="fa fa-dashboard"></i>&nbsp;&nbsp;&nbsp;Dashboard </a>--}}
+{{--        </li>--}}
+{{--        @foreach($roles as $role)--}}
+{{--            @foreach($role->users as $user)--}}
+{{--                @if(Auth::user()->id == $user->id)--}}
+{{--                    <h3 class="menu-title">{{ $role->title }}</h3><!-- /.menu-title -->--}}
+{{--                    @if(count($role->permissions)>0)--}}
+{{--                        @foreach($role->permissions as $permission)--}}
+{{--                            @if($permission ->name != "Logout")--}}
+{{--                                <li class="menu-item-has-children dropdown">--}}
+{{--                                    @if($permission->parent == 0)--}}
+{{--                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">&nbsp;&nbsp;&nbsp;{!! $permission->icon !!}&nbsp;&nbsp;&nbsp;{{ $permission->name }}</a>--}}
+{{--                                        <ul class="sub-menu children dropdown-menu">--}}
+{{--                                            @foreach($role->permissions as $permission_c)--}}
+{{--                                                @if($permission_c->parent == $permission->id )--}}
+{{--                                                    @if($permission_c ->name != "Logout")--}}
+{{--                                                    <li><a href="{{ route( $permission_c->url ) }}">{!! $permission_c->icon !!}{{ $permission_c->name }}</a></li>--}}
+{{--                                                    @else--}}
+{{--                                                    <li>--}}
+{{--                                                        <a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();"><i class="fas fa-sign-out-alt"></i><span class="ml-1">{{ __('Logout') }}</span></a>--}}
+{{--                                                    </li>--}}
+{{--                                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">--}}
+{{--                                                        @csrf--}}
+{{--                                                    </form>--}}
+{{--                                                    @endif--}}
+{{--                                                @endif--}}
+{{--                                            @endforeach--}}
+{{--                                        </ul>--}}
+{{--                                    @endif--}}
+{{--                                </li>--}}
+{{--                            @else--}}
+
+{{--                            @endif--}}
+{{--                        @endforeach--}}
+{{--                    @else--}}
+
+{{--                    @endif--}}
+
+{{--                @else--}}
+
+{{--                @endif--}}
+{{--            @endforeach--}}
+{{--        @endforeach--}}
+{{--    </ul>--}}
+{{--</div><!-- /.navbar-collapse -->--}}
